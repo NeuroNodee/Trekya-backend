66a3cc3226eac9f3c8f041b3f9b1d03bb0df9102
Kunjan Bhatt <bhattakunjan10@gmail.com>
2026-01-12 11:50:24 +0545

    Final addition of ashok branch jan12

--- backend/settings.py
+++ backend/settings.py
@@ -171,8 +171,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'neuronodeai@gmail.com'
-EMAIL_HOST_PASSWORD = 'pdxw anxh iria zucx'
+EMAIL_HOST_USER =config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER = 'neuronodeai@gmail.com'
+# EMAIL_HOST_PASSWORD = 'pdxw anxh iria zucx'
 
 
 # Modern allauth configuration (replaces deprecated settings)
