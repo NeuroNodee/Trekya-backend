fdb1dbf5ce3ec6d08a568904576286e643ee5433
Kunjan Bhatt <bhattakunjan10@gmail.com>
2026-01-24 17:57:14 +0545

    working on travel kit views

--- travelKit/urls.py
+++ travelKit/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('AllLocation/', views.getAllLocation, name='getAllLocation'),
     path('AllTravelKitItems/', views.getAllTravelKitItems, name='getAllTravelKitItems'),
+    path('TravelKitInfo/', views.getTravelKitInfo, name='getTravelKitInfo'),
 ]
--- travelKit/views.py
+++ travelKit/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Location, TravelKitItem
+from .models import Location, TravelKitItem, TravelKit
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -9,13 +9,23 @@ from rest_framework.decorators import permission_classes
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def getAllLocation(request):
+    """Get all locations"""
     locations = Location.objects.all().values()
     return Response({ "message": "Success", "data": list(locations) })
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def getAllTravelKitItems(request):
+    """Get all travel kit items"""
     travel_kits = TravelKitItem.objects.all().values()
     return Response({ "message": "Success", "data": list(travel_kits) })
 
-
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getTravelKitInfo(request):
+    """Get specific travel kit info by location"""
+    location = request.GET.get('location')
+    if not location:
+        return Response({ "message": "Location is required" })
+    travel_kit = TravelKit.objects.filter(locations__name=location).values()
+    return Response({ "message": "Success", "data": list(travel_kit) })
